3274f41f26391b8cada8ab226056410a7f0f3114
Yusuf <yusufisaabdullatif@gmail.com>
2024-01-08 08:57:32 +0700

    initial commit

--- registerPasien.php
+++ registerPasien.php
@@ -34,42 +34,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     } else {
         $error = "nama sudah digunakan";
     }
-
-    /*if ($no_ktp === $confirm_ktp) {
-        $query = "SELECT * FROM pasien WHERE nama = '$nama'";
-        $result = $mysqli->query($query);
-
-        if ($result === false) {
-            die("Query error: " . $mysqli->error);
-        }
-
-        if ($result->num_rows == 0) {
-            $hashed_ktp = password_hash($no_ktp, PASSWORD_DEFAULT);
-
-            // Get the total number of pasien
-            $result = mysqli_query($mysqli, "SELECT COUNT(*) as total FROM pasien");
-            $row = mysqli_fetch_assoc($result);
-            $totalPasien = $row['total'];
-
-            // Generate no_rm based on the current date and total number of pasien
-            $no_rm = date('Ymd') . '-' . ($totalPasien + 1);
-
-            $insert_query = "INSERT INTO pasien (nama, no_ktp, alamat, no_hp, no_rm) VALUES ('$nama', '$hashed_ktp', '$alamat', '$no_hp', '$no_rm')";
-            if (mysqli_query($mysqli, $insert_query)) {
-                $_SESSION['no_rm'] = $no_rm; // Store no_rm in session
-                echo "<script>
-                alert('Pendaftaran Berhasil. Nomor RM Anda adalah: $no_rm'); 
-                document.location='index.php?page=loginPasien';
-                </script>";
-            } else {
-                $error = "Pendaftaran gagal";
-            }
-        } else {
-            $error = "nama sudah digunakan";
-        }
-    } else {
-        $error = "No.KTP tidak cocok";
-    }*/
 }
 ?>
 <div class="container mt-5">
@@ -93,17 +57,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                             <input type="text" name="nama" class="form-control" required placeholder="Masukkan nama anda">
                         </div>
                         <div class="form-group">
-                            <label for="alamat">alamat</label>
+                            <label for="alamat">Alamat</label>
                             <input type="text" name="alamat" class="form-control" required placeholder="Masukkan alamat anda">
                         </div>
                         <div class="form-group">
-                            <label for="no_ktp">No_KTP</label>
-                            <input type="no_ktp" name="no_ktp" class="form-control" required placeholder="Masukkan password">
+                            <label for="no_ktp">Nomor KTP</label>
+                            <input type="no_ktp" name="no_ktp" class="form-control" required placeholder="Masukkan nomor KTP">
                         </div>
-                        <!--<div class="form-group">
-                            <label for="confirm_ktp">Confirm KTP</label>
-                            <input type="password" name="confirm_ktp" class="form-control" required placeholder="Masukkan password konfirmasi">
-                        </div>-->
                         <div class="form-group">
                             <label for="no_hp">No.HP</label>
                             <input type="text" name="no_hp" class="form-control" required placeholder="Masukkan no_hp anda">
